882faeff1eeef5fcdd73814e76d35a304bbb3039
dtinth on MBP M1 <dtinth@spacet.me>
2024-06-02 20:33:27 +0700

    backfill all data

---
+++ src/loadAll.ts
@@ -0,0 +1,15 @@
+import { $ } from "bun";
+// Loop from 2023-08-01 to today
+let date = new Date("2023-08-01T00:00:00Z");
+const today = new Date(Date.now() - 12 * 3600e3);
+while (date < today) {
+  const dateString = date.toISOString().slice(0, 10);
+  console.log(`::group::Loading data for ${dateString}`);
+  try {
+    await $`node src/load.js ${dateString}`;
+  } catch (error) {
+    console.error(error);
+  }
+  console.log(`::endgroup::`);
+  date.setDate(date.getDate() + 1);
+}
