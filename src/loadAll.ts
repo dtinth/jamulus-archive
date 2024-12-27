6c783a30b7614095e323dc4a12c3f1be6db1160d
Thai Pangsakulyanont <dtinth@spacet.me>
2024-12-27 13:03:51 +0700

    backfill

--- src/loadAll.ts
+++ src/loadAll.ts
@@ -1,6 +1,6 @@
 import { $ } from "bun";
 
-let date = new Date(Date.now() - 7 * 24 * 3600e3);
+let date = new Date(Date.now() - 60 * 24 * 3600e3);
 date.setUTCHours(0, 0, 0, 0);
 
 const today = new Date(Date.now() - 12 * 3600e3);
